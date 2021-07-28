4189e9a8707a9835095119952bd737a3f7f54a04
Baskar M <hi@baskar.co.in>
2021-07-29 01:10:49 +0530

    Null safety adddedd

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  WifiInfoWrapper _wifiObject;
+  WifiInfoWrapper? _wifiObject;
 
   @override
   void initState() {
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    WifiInfoWrapper wifiObject;
+    WifiInfoWrapper? wifiObject;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
       wifiObject = await WifiInfoPlugin.wifiDetails;
@@ -36,7 +36,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     String ipAddress =
-        _wifiObject != null ? _wifiObject.ipAddress.toString() : "ip";
+        _wifiObject != null ? _wifiObject!.ipAddress.toString() : "ip";
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
